fdd9b8643fcd87010b8bc9772fc7baa6c0df46cb
Jangelo14 <joao.fogaca.ti@gmail.com>
2026-05-02 02:01:25 -0300

    Resolvido o Desafio-17

--- Desafios/Desafio-17.py
+++ Desafios/Desafio-17.py
@@ -1,3 +1,15 @@
 #Aula 08 - Utilizando Modulo
 
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retangulo. Calcule e mostre o comprimento da hipotenusa
+from math import hypot
+co = float(input("Qual o valor do cateto oposto? "))
+ca = float(input("Qual o valor do cato adjacente? "))
+
+#Resolvendo sem utilizar o modulo
+#hi = (co ** 2 + ca ** 2) ** (1/2)
+
+#print("O valor da hipotenusa vai ser {:.2f}".format(co, ca, hi))
+
+#Resolvendo utilizando o modulo
+hi = hypot(co, ca)
+print("O valor da hipotenusa vai ser {:.2f}".format(hi))
