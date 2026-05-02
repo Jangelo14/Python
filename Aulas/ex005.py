b1cd5b5a0286cb419cbe263157067cb0e23af8e5
Jangelo14 <joao.fogaca.ti@gmail.com>
2026-05-02 02:02:29 -0300

    Adicionado a aula 08 - Utilizando modulos. Junto com uma explicação de como utilizar

---
+++ Aulas/ex005.py
@@ -0,0 +1,9 @@
+#Aula 08 - Utilizando Modulo
+import math #Para importar um modulo especifico da biblioteca, é possivel utilizando o import {nome da biblioteca} from {o modulo escolhido}
+num = int(input("Digite um numero: "))
+
+raiz = math.sqrt(num)
+
+print("A raiz de {}, é igual a {}".format(num, raiz))
+
+#É possivel instalar novas bibliotecas que não está disponivel por padrão no python, utilizando o pip install e a biblioteca que quer adicionar
