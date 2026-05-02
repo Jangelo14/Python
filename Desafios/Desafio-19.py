4380cd3e811d0b9245fbd264331b0410e530aa6b
Jangelo14 <joao.fogaca.ti@gmail.com>
2026-05-02 02:32:17 -0300

    Adicionado e resolvido o Desafio-19

---
+++ Desafios/Desafio-19.py
@@ -0,0 +1,12 @@
+#Aula 08 - Utilizando Modulo
+
+#Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido
+from random import choice
+n1 = input("Digite o nome do primeiro aluno: ")
+n2 = input("Digite o nome do segundo aluno: ")
+n3 = input("Digite o nome do terceiro aluno: ")
+n4 = input("Digite o nome do quarto aluno: ")
+list = [n1, n2, n3, n4]
+escolhido = choice(list)
+
+print("o aluno selecionado foi {}".format(escolhido))
