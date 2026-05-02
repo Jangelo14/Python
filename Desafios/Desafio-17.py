cd6138faad45b813ee1f04546d72ea5c5271b31f
Jangelo14 <joao.fogaca.ti@gmail.com>
2026-05-02 01:44:16 -0300

    Adicionado e resolvido o Desafio-17

---
+++ Desafios/Desafio-17.py
@@ -0,0 +1,3 @@
+#Aula 08 - Utilizando Modulo
+
+#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retangulo. Calcule e mostre o comprimento da hipotenusa
