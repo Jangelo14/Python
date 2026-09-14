2afcd77c81055c3ba3bbdf5b7614f6d43cada45b
Jangelo14 <joao.fogaca.ti@gmail.com>
2026-09-13 22:09:08 -0300

    Adicionado e resolvido o Desafio-23

---
+++ Desafios/Desafio-23.py
@@ -0,0 +1,17 @@
+#Faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados:
+#Ex. Digite o numero: 1985
+#Unidade: 5
+#Dezena: 8
+#Centena: 9
+#Milhar: 1
+
+num = int(input("Infome um numero: "))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print("Analisando o numero {}".format(num))
+print("Unidade: {}".format(u))
+print("Dezena: {}".format(d))
+print("Centena: {}".format(c))
+print("Milhar: {}".format(m))
