79fe3d4549fa7f6f06f81c9de5908ad2ca5fc85b
Jangelo14 <joao.fogaca.ti@gmail.com>
2026-09-13 22:10:08 -0300

    Instruções sobre a aula 09

---
+++ Aulas/ex006.py
@@ -0,0 +1,10 @@
+#Aula 09 - Manipulação de texto
+frase = 'Curso em Video Python'
+print(frase[:20])
+#É possivel manipular textos dentro do python com algumas atribuições, sendo elas:
+#Len: para verificar a quantidade de espaços que a string possui
+#Strip: para remover espaços indesejados do texto (famosos espaços em branco), exceto espaços no meio da frase
+#Count: para contar a quantidade de vezes que uma palavra informada aparecem na frase
+#Uper: para deixar todos os caracteres em maiusculos
+#Lower: para deixar todos os caracteres em minusculos
+#Replace: utilizado para alterar o texto que será apresentado (ex. Curso em video (Colocando .replace("Video","Musica") => Curso em Musica:
