7a49bd94d31f46819b053c606ea3b80e6ef8a481
Jangelo14 <joao.fogaca.ti@gmail.com>
2026-09-13 22:55:34 -0300

    Incluído e Resolvido Desafio-27

---
+++ Desafios/Desafio-27.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente:
+#Ex. Ana Maria de Souza
+#Primeiro nome: Ana
+#Ultimo: Souza
+
+n = str(input("Digite seu nome: ")).strip()
+nome = n.split()
+print("Seu primeiro nome é: {}".format(nome[0]))
+print("Seu ultimo nome é: {}".format(nome[len(nome)-1]))
